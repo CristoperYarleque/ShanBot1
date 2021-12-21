70c3b5c77b679443c4d330d4dab7093f0a1cfa9a
CristoperYarleque <caypsaturno@gmail.com>
2021-12-20 20:02:55 -0500

    chore: bot wpp

---
+++ src/version.js
@@ -0,0 +1,23 @@
+const version = (prefix, pushname) => {
+    return `
+*ShanBot by fenix*
+
+*Actualizado:* 03 de Diciembre del 2021
+*Versión actual:* 4.4
+*Ofrecida por:* fenix™
+
+
+*INFORME*
+
+Si no funciona el comando *play o *play2 checa el blog del bot y mira la version que poses 
+Ya que ire cambiando mi apikey de mi bot para que siga en funcionamiento
+Cambiare la apikey pasando un mes o si veo muchos atercados o robos
+Si vez a alguien que robo mi bot comunicate conmigo con el comando *creador
+
+
+_*by fenix*_
+`
+
+}
+
+exports.version = version
